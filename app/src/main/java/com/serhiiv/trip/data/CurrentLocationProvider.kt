5c7959642e2b0edbf54d4e8199667e7e0088f531
serhii.v <skit078@gmail.com>
2019-09-17 15:04:57 +0300

    fixed obtains current location

--- app/src/main/java/com/serhiiv/trip/data/CurrentLocationProvider.kt
+++ app/src/main/java/com/serhiiv/trip/data/CurrentLocationProvider.kt
@@ -1,23 +1,27 @@
 package com.serhiiv.trip.data
 
+import android.app.Activity
+import android.location.Location
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.model.LatLng
-import com.serhiiv.trip.App
-import javax.inject.Inject
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 import kotlin.coroutines.suspendCoroutine
 
-class CurrentLocationProvider @Inject constructor(app: App) {
+class CurrentLocationProvider constructor(activity: Activity) {
     private val fusedLocationClient: FusedLocationProviderClient by lazy {
-        LocationServices.getFusedLocationProviderClient(app.context)
+        LocationServices.getFusedLocationProviderClient(activity)
     }
 
     suspend fun getCurrentPosition(): LatLng = suspendCoroutine { continuation ->
         fusedLocationClient.lastLocation
-            .addOnSuccessListener {
-                continuation.resume(LatLng(it.latitude, it.longitude))
+            .addOnSuccessListener { location: Location? ->
+                if (location == null) {
+                    continuation.resumeWithException(Exception("Location is null"))
+                } else {
+                    continuation.resume(LatLng(location.latitude, location.longitude))
+                }
             }
             .addOnFailureListener {
                 continuation.resumeWithException(it)
--- app/src/main/java/com/serhiiv/trip/di/fetures/FeatureMapsModule.kt
+++ app/src/main/java/com/serhiiv/trip/di/fetures/FeatureMapsModule.kt
@@ -1,13 +1,24 @@
 package com.serhiiv.trip.di.fetures
 
+import com.serhiiv.trip.data.CurrentLocationProvider
 import com.serhiiv.trip.di.PerActivity
 import com.serhiiv.trip.maps.MapsActivity
 import dagger.Module
+import dagger.Provides
 import dagger.android.ContributesAndroidInjector
 
 @Module
 interface FeatureMapsModule {
     @PerActivity
-    @ContributesAndroidInjector
+    @ContributesAndroidInjector(modules = [ProvidesModule::class])
     fun contributeMapsActivity(): MapsActivity
+
+    @Module
+    class ProvidesModule {
+        @Provides
+        @PerActivity
+        fun provideCurrentLocationProvider(activity: MapsActivity): CurrentLocationProvider {
+            return CurrentLocationProvider(activity)
+        }
+    }
 }
